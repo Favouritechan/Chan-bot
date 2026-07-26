2bc0581cd418e8f88a5291ff1e59847b4c519f68
Favouritechan <favournwoku@gmail.com>
2026-07-26 11:07:16 +0100

    Add balance command to check user wallet and bank

---
+++ commands/economy/balance.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const User = require("../../models/User");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("balance")
+        .setDescription("Check your wallet and bank balance."),
+
+    async execute(interaction) {
+
+        let user = await User.findOne({
+            userId: interaction.user.id
+        });
+
+        if (!user) {
+            user = await User.create({
+                userId: interaction.user.id
+            });
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor("#FFD700")
+            .setTitle("💰 Balance")
+            .setThumbnail(interaction.user.displayAvatarURL())
+            .addFields(
+                {
+                    name: "👛 Wallet",
+                    value: `₦${user.wallet.toLocaleString()}`,
+                    inline: true
+                },
+                {
+                    name: "🏦 Bank",
+                    value: `₦${user.bank.toLocaleString()}`,
+                    inline: true
+                }
+            )
+            .setFooter({
+                text: `Requested by ${interaction.user.username}`
+            })
+            .setTimestamp();
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+
+    }
+};
