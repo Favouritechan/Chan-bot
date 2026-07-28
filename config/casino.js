209750b8ab95caa85bfd372a8bb971da3a1bf012
Favouritechan <favournwoku@gmail.com>
2026-07-29 00:09:12 +0100

    Add casino configuration for roulette and payouts

---
+++ config/casino.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+    // Roulette
+    roulette: {
+
+        minimumBet: 100,
+
+        bettingTime: 20,
+
+        spinningTime: 10,
+
+        cooldown: 2000,
+
+        jackpotChance: 0.01
+
+    },
+
+    payouts: {
+
+        red: 2,
+
+        black: 2,
+
+        "1-18": 2,
+
+        "19-36": 2,
+
+        "1-12": 3,
+
+        "13-24": 3,
+
+        "25-36": 3,
+
+        straight: 10
+
+    }
+
+};
