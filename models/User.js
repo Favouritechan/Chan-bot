38f8c707f160bbd42b32cdb615bbd7154512a7b0
Favouritechan <favournwoku@gmail.com>
2026-07-29 00:20:27 +0100

    Update User schema with new fields and structure
    Refactor user schema to include additional properties and restructure existing fields.

--- models/User.js
+++ models/User.js
@@ -1,95 +1,101 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true
-  },
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
 
-  wallet: {
-    type: Number,
-    default: 1000
-  },
+    wallet: {
+        type: Number,
+        default: 1000
+    },
 
-  bank: {
-    type: Number,
-    default: 0
-  },
-  
-casino: {
-    gamesPlayed: {
+    bank: {
         type: Number,
         default: 0
     },
 
-    totalBet: {
+    casino: {
+        gamesPlayed: {
+            type: Number,
+            default: 0
+        },
+
+        totalBet: {
+            type: Number,
+            default: 0
+        },
+
+        totalWon: {
+            type: Number,
+            default: 0
+        },
+
+        totalLost: {
+            type: Number,
+            default: 0
+        },
+
+        biggestWin: {
+            type: Number,
+            default: 0
+        },
+
+        biggestLoss: {
+            type: Number,
+            default: 0
+        },
+
+        currentStreak: {
+            type: Number,
+            default: 0
+        },
+
+        bestStreak: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    job: {
+        type: String,
+        default: "Unemployed"
+    },
+
+    inventory: {
+        type: [String],
+        default: []
+    },
+
+    lastDaily: {
         type: Number,
         default: 0
     },
 
-    totalWon: {
+    level: {
+        type: Number,
+        default: 1
+    },
+
+    xp: {
         type: Number,
         default: 0
     },
 
-    totalLost: {
-        type: Number,
-        default: 0
+    lastWork: {
+        type: Date,
+        default: null
     },
 
-    biggestWin: {
-        type: Number,
-        default: 0
-    },
-
-    biggestLoss: {
-        type: Number,
-        default: 0
-    },
-
-    currentStreak: {
-        type: Number,
-        default: 0
-    },
-
-    bestStreak: {
-        type: Number,
-        default: 0
+    lastCollect: {
+        type: Date,
+        default: null
     }
-},
-  job: {
-    type: String,
-    default: "Unemployed"
-  },
 
-  inventory: {
-    type: [String],
-    default: []
-  },
+}, {
+    timestamps: true
+});
 
-  lastDaily: {
-    type: Number,
-    default: 0
-  },
-
-  level: {
-    type: Number,
-    default: 1
-  },
-
-  xp: {
-    type: Number,
-    default: 0
-  },
-
-  lastWork: {
-    type: Date,
-    default: null
-  },
-
-  lastCollect: {
-    type: Date,
-    default: null
-  },
-
+module.exports = mongoose.model("User", userSchema);
